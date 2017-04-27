61b963e3a94e5afb8adc7cc54e9090c5e0e47c25
Dimitris Housiadas <housiadas@gmail.com>
2017-04-27 18:36:41 +0300

    trying to fix bug

--- run-natively.sh
+++ run-natively.sh
@@ -13,4 +13,29 @@ then
     exit 1
 fi
 
+if ! isNpmPackageInstalled grunt-cli
+then
+    echo Installing Grunt, you may be asked for your password to run sudo...
+    sudo npm install -g grunt-cli #CMD
+else
+    echo Grunt is already installed, skipping...
+fi
+
+if ! isNpmPackageInstalled bower
+then
+    echo Installing Bower, you may be asked for your password to run sudo...
+    sudo npm install -g bower #CMD
+else
+    echo Bower is already installed, skipping...
+fi
+
+echo Installing npm dependencies...
+npm install #CMD
+
+echo Installing Bower dependencies...
+bower install #CMD
+
+echo Building the console...
+grunt build #CMD
+
 ./gradlew shim-server:bootRun #CMD
